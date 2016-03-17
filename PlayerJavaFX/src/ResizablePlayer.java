4825fec4110cbf0d17b95246d3c35876b594d8e6
Thomas Fouan <thomasfouan@MacBook-Air-de-Thomas.local>
2016-03-17 10:04:13 +0100

    Ajout du PlayerJavaFX

---
+++ PlayerJavaFX/src/ResizablePlayer.java
@@ -0,0 +1,152 @@
+import com.sun.jna.Memory;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.beans.property.FloatProperty;
+import javafx.beans.property.SimpleFloatProperty;
+import javafx.event.EventHandler;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.image.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import uk.co.caprica.vlcj.component.DirectMediaPlayerComponent;
+import uk.co.caprica.vlcj.discovery.NativeDiscovery;
+import uk.co.caprica.vlcj.player.direct.BufferFormat;
+import uk.co.caprica.vlcj.player.direct.BufferFormatCallback;
+import uk.co.caprica.vlcj.player.direct.DirectMediaPlayer;
+import uk.co.caprica.vlcj.player.direct.format.RV32BufferFormat;
+
+import java.nio.ByteBuffer;
+
+/**
+ * Created by thomasfouan on 16/03/2016.
+ */
+public class ResizablePlayer extends Application {
+
+    private static final String PATH_TO_VIDEO = "/Users/thomasfouan/Desktop/video.mp4";//"/Volumes/Transcend/Film et séries/Arrow - Saison 4/Arrow.S04E01.REPACK.VOSTFR.720p.HDTV.x264-ZT.zone-telechargement.com.mkv";
+
+    private ImageView imageView;
+
+    private DirectMediaPlayerComponent mediaPlayerComponent;
+
+    private WritableImage writableImage;
+
+    private Pane playerHolder;
+
+    private WritablePixelFormat<ByteBuffer> pixelFormat;
+
+    private FloatProperty videoSourceRatioProperty;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        mediaPlayerComponent = new CanvasPlayerComponent();
+        playerHolder = new Pane();
+        videoSourceRatioProperty = new SimpleFloatProperty(0.4f);
+        pixelFormat = PixelFormat.getByteBgraPreInstance();
+        initializeImageView();
+        Scene scene = new Scene(new BorderPane(playerHolder));
+        primaryStage.setScene(scene);
+        mediaPlayerComponent.getMediaPlayer().prepareMedia(PATH_TO_VIDEO);
+        mediaPlayerComponent.getMediaPlayer().start();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent event) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+
+        primaryStage.show();
+    }
+
+    private void initializeImageView() {
+        Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+        writableImage = new WritableImage((int) visualBounds.getWidth(), (int) visualBounds.getHeight());
+
+        imageView = new ImageView(writableImage);
+        playerHolder.getChildren().add(imageView);
+
+        playerHolder.widthProperty().addListener((observable, oldValue, newValue) -> {
+            fitImageViewSize(newValue.floatValue(), (float) playerHolder.getHeight());
+        });
+
+        playerHolder.heightProperty().addListener((observable, oldValue, newValue) -> {
+            fitImageViewSize((float) playerHolder.getWidth(), newValue.floatValue());
+        });
+
+        videoSourceRatioProperty.addListener((observable, oldValue, newValue) -> {
+            fitImageViewSize((float) playerHolder.getWidth(), (float) playerHolder.getHeight());
+        });
+    }
+
+    private void fitImageViewSize(float width, float height) {
+        Platform.runLater(() -> {
+            float fitHeight = videoSourceRatioProperty.get() * width;
+            if (fitHeight > height) {
+                imageView.setFitHeight(height);
+                double fitWidth = height / videoSourceRatioProperty.get();
+                imageView.setFitWidth(fitWidth);
+                imageView.setX((width - fitWidth) / 2);
+                imageView.setY(0);
+            }
+            else {
+                imageView.setFitWidth(width);
+                imageView.setFitHeight(fitHeight);
+                imageView.setY((height - fitHeight) / 2);
+                imageView.setX(0);
+            }
+        });
+    }
+
+    private class CanvasPlayerComponent extends DirectMediaPlayerComponent {
+
+        public CanvasPlayerComponent() {
+            super(new CanvasBufferFormatCallback());
+        }
+
+        PixelWriter pixelWriter = null;
+
+        private PixelWriter getPW() {
+            if (pixelWriter == null) {
+                pixelWriter = writableImage.getPixelWriter();
+            }
+            return pixelWriter;
+        }
+
+        @Override
+        public void display(DirectMediaPlayer mediaPlayer, Memory[] nativeBuffers, BufferFormat bufferFormat) {
+            if (writableImage == null) {
+                return;
+            }
+            Platform.runLater(() -> {
+                Memory nativeBuffer = mediaPlayer.lock()[0];
+                try {
+                    ByteBuffer byteBuffer = nativeBuffer.getByteBuffer(0, nativeBuffer.size());
+                    getPW().setPixels(0, 0, bufferFormat.getWidth(), bufferFormat.getHeight(), pixelFormat, byteBuffer, bufferFormat.getPitches()[0]);
+                }
+                finally {
+                    mediaPlayer.unlock();
+                }
+            });
+        }
+    }
+
+    private class CanvasBufferFormatCallback implements BufferFormatCallback {
+        @Override
+        public BufferFormat getBufferFormat(int sourceWidth, int sourceHeight) {
+            Rectangle2D visualBounds = Screen.getPrimary().getVisualBounds();
+            Platform.runLater(() -> videoSourceRatioProperty.set((float) sourceHeight / (float) sourceWidth));
+            return new RV32BufferFormat((int) visualBounds.getWidth(), (int) visualBounds.getHeight());
+        }
+    }
+
+    public static void main(String[] args) {
+        new NativeDiscovery().discover();
+        Application.launch(ResizablePlayer.class);
+    }
+}
